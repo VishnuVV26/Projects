4a09299e054c9eb8d617ff52d30e83babc5c6e5c
my-name <my-email>
2023-12-29 13:29:38 +0530

    added post data form

--- myapp/src/Components/Content/Home.js
+++ myapp/src/Components/Content/Home.js
@@ -15,12 +15,26 @@ const Home = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [postperpage, setPostperPage] = useState(10);
     const [searchTerm, setSearchTerm] = useState('');
+    const [nextUserId, setNextUserId] = useState(null);
+
+    const getNextUserId = (users) => {
+        const maxId = users.reduce((max, user) => (user.id > max ? user.id : max), 0);
+        return maxId + 1;
+      };
+    
+    const[newUserData,SetnewUserData]=useState({
+        firstname:'',
+        lastname:'',
+        email:'',
+        phone:'',
+    })
 
 
 
     React.useEffect(() => {
         axios.get(url).then((res) => {
             setData(res.data);
+            setNextUserId(getNextUserId(res.data));
         })
     }, [])
 
@@ -35,6 +49,23 @@ const Home = () => {
     );
 
     if (!Data) return null;
+
+const handleFormSubmit=()=>{
+        SetnewUserData({...newUserData,id:nextUserId})
+    console.log("id:",nextUserId);
+    axios.post(url,newUserData).then((res)=>{
+        setData([...Data,res.data]);
+        console.log(res.data);
+        setNextUserId(nextUserId+1);
+      SetnewUserData({
+            firstname:'',
+            lastname:'',
+            email:'',
+            phone:'',
+        })
+    })
+}
+
     return (
         <div className='Data'>
 
@@ -73,6 +104,28 @@ const Home = () => {
                         ))}
                     </tbody>
                 </table>
+<form className='form-user'>
+                <label>
+                    First Name
+                    <input type='text' placeholder='First name' value={newUserData.firstname} onChange={(e)=>SetnewUserData({...newUserData,firstname:e.target.value})}/><br/>
+                </label>
+                <label>
+                Last Name
+                    <input type='text' placeholder='Last name' value={newUserData.lastname} onChange={(e)=>SetnewUserData({...newUserData ,lastname:e.target.value})}/><br/>
+                </label>
+                <label>
+                    Email
+                    <input type='text' placeholder='Email'value={newUserData.email} onChange={(e)=>SetnewUserData({...newUserData,email:e.target.value})}/><br/>
+                </label>
+                <label>
+                    Number
+                    <input type='text' placeholder='Number' value={newUserData.phone} onChange={(e)=>SetnewUserData({...newUserData,phone:e.target.value})}/><br/>
+                </label><br/>
+                <button type='submit' className='submit-1' onClick={(e)=>{
+                    e.preventDefault();
+                    handleFormSubmit();
+                }}>submit</button>
+                </form>
             </div>
             <div>
                 <Pagination totalposts={Data.length}
